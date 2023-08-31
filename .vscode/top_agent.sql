b461f328af0f977cb828015ad1b30cd2478188f0
Katharine Zarbock <kjzarbock@gmail.com>
2023-08-31 17:56:11 -0500

    top agent

---
+++ .vscode/top_agent.sql
@@ -0,0 +1,8 @@
+SELECT e.FirstName || ' ' || e.LastName AS EmployeeFullName, SUM(i.Total) AS TotalSales
+FROM Employee AS e
+JOIN Customer AS c ON e.EmployeeId = c.SupportRepId
+JOIN Invoice AS i ON c.CustomerId = i.CustomerId
+WHERE e.Title = 'Sales Support Agent'
+GROUP BY e.EmployeeId
+ORDER BY TotalSales DESC
+LIMIT 1;
